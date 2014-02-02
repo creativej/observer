ba505b317f5ee27eb5712c7ff4f64945cfaaa890
creativej <lu.jamison@gmail.com>
2014-02-02 19:40:21 +1100

    Reencrypt all connection password

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20131013104146) do
+ActiveRecord::Schema.define(:version => 20140202082858) do
 
   create_table "connections", :force => true do |t|
     t.integer  "user_id"
