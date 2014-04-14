41d071ef65c11b8dc1d2dd0e80eb7e23d477a210
creativej <lu.jamison@gmail.com>
2014-04-14 16:48:23 +0800

    Adding comments to widget.js

--- app/assets/javascripts/modules/widget.js
+++ app/assets/javascripts/modules/widget.js
@@ -5,6 +5,10 @@
 (function($, Observer, window) {
 	'use strict';
 
+	/**
+	 * Widget
+	 * A class for encapsulating widget behaviour in nice methods
+	 **/
 	Observer.modules.widget = function($el, options) {
 		var instance = window.eventable({
 				dataSets: [],
@@ -14,6 +18,11 @@
 			})
 			;
 
+		/**
+		 * Widget.autoRefresh = True
+		 * Widget.refresh = 5 minutes
+		 * Widget.chartOptions = {}
+		 */
 		instance.options = $.extend({
 			refresh: 5 * 60 * 1000, // Refresh every 5 minutes
 			autoRefresh: 1,
@@ -45,16 +54,28 @@
 			loadData(instance.loadingQueue.shift());
 		}
 
+		/**
+		 * Widget#dataReady(callback) -> Widget
+		 * - callback (Function): callback function
+		 **/
 		instance.dataReady = function(callback) {
 			return this.on('ready.data', callback);
 		};
 
+		/**
+		 * Widget#addClass(cls) -> Widget
+		 * - cls (string): Add class/es to widget's DOM
+		 **/
 		instance.addClass = function(cls) {
 			this.$el.addClass(cls);
 			return this;
 		};
 
-		instance.load = function(urls, options) {
+		/**
+		 * Widget#load(dataOption) -> Widget
+		 * dataOption (Object | Array): Data options
+		 **/
+		instance.load = function(urls) {
 			var widgetDataOptions = urls.slice(0);
 
 			if (Array.isArray(widgetDataOptions)) {
@@ -68,6 +89,11 @@
 			return this;
 		};
 
+
+		/**
+		 * Widget#refresh(ms) -> Widget
+		 * - ms (int): 1000 = second
+		 **/
 		instance.refresh = function(ms) {
 			if (!ms) { this.refreshTimer = null; return this; }
 
@@ -78,21 +104,39 @@
 			return this;
 		};
 
+
+		/**
+		 * Widget#jqplot(id, dataOptions, [options]) -> JqplotWidget
+		 * - id (string): Id to the dom
+		 * - dataOptions (Object | Array): Data options
+		 **/
 		instance.jqplot = function(id, dataSets, options) {
 			return Observer.jqplot(id, dataSets, options);
 		};
 
+		/**
+		 * Widget#setChartOptions(options) -> Widget
+		 * - options (Object)
+		 **/
 		instance.setChartOptions = function (options) {
 			this.options.chartOptions = options;
 			return this;
 		};
 
+		/**
+		 * Widget#applyBindings(model) -> Widget
+		 * - model (Object): Knockout model. For detail reference visit: www.knockoutjs.com
+		 **/
 		instance.applyBindings = function(model) {
 			window.ko.applyBindings(model, this.$el.get(0));
 			return this;
 		};
 
-		instance.loadDateChart = function(widgetDataOptions, callback) {
+		/**
+		 * Widget#loadDateChart(dataOptions) -> Widget
+		 * - dataOptions (Object | Array): Data Options
+		 **/
+		instance.loadDateChart = function(widgetDataOptions) {
 			return Observer.modules.dateChartWidget(this)
 				.loadAndDraw(widgetDataOptions);
 		};
